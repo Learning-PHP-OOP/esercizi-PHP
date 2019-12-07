4f621b275322e83905d1c5933f0231177dc9faf5
valentina <valentinagiannone@gmail.com>
2019-12-07 13:40:32 +0100

    snack PHP svolti in classe

--- b_usoGet/index.php
+++ b_usoGet/index.php
@@ -2,26 +2,32 @@
 <html lang="en" dir="ltr">
  <head>
   <meta charset="utf-8">
-  <title></title>
-  <?php
-  //secondo esercizio uso di $_GET commentato per far ordine
-  //$userName = $_GET["name"];
-  //sotto scrivo : <?php var_dump($_GET);(qui
-  //chiudo il tag php) non lo metto perchè si scommenta)
-  //<h1>
-  // La mia fidanzata è :
-  // <?php echo $userName; (chiusura tag php)
-  //</h1>
-  //sul link metto chiave=valore , chiave in  uesto caso è "name" e il valore
-  //lo passo io tipo : http://localhost:8888/&name=minnie.
-   ?>
-   <?php
-   //terzo esercizio
-    //Passare due argomenti allo script: Nome e Cognome.
-    //Lo script dovrà salutare l’interlocutore
+  <title>GET</title>
+
+  <!--SECONDO ESERCIZIO USO DI : $_GET
+  ______________________________________
+  //TO REMEMBER : sul link metto chiave=valore , chiave in  uesto caso è
+  "name" e il valore lo passo io tipo : http://localhost:8888/&name=minnie.
+
+
+   $userName = $_GET["name"];
+
+   output :
+   <?php var_dump($_GET);?>
+   <h1>La mia fidanzata è : <?php echo $userName; ?></h1>
+
+
+
+   TERZO ESERCIZIO
+   _______________________________________
+    Passare due argomenti allo script: Nome e Cognome.
+    //Lo script dovrà salutare l’interlocutore-->
+
+    <?php
     $myName= "Daenerys";
     $mySurname = "Targarien";
     ?>
+
  </head>
 
  <body>
